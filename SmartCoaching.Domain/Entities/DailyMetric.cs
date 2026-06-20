3b25dc324d7fae0d55a572a94e6261061430daea
melo <melih@DESKTOP-MRA5AHM>
2026-06-20 22:43:38 +0300

    Sporcu günlük verilerini tutacak olan DailyMetric varlığı eklendi

---
+++ SmartCoaching.Domain/Entities/DailyMetric.cs
@@ -0,0 +1,33 @@
+﻿using SmartCoaching.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartCoaching.Domain.Entities
+{
+    public class DailyMetric : BaseEntity
+    {
+        private DailyMetric()
+        {
+        }
+
+        public DailyMetric(Guid athleteId, decimal weight, int caloriesConsumed, int stepCount, int fatigueScore ,string? notes = null)
+        {
+            AthleteId = athleteId;
+            Weight = weight;
+            CaloriesConsumed = caloriesConsumed;
+            StepCount = stepCount;
+            FatigueScore = fatigueScore;
+            Notes = notes;
+
+        }
+
+        public Guid AthleteId { get; private set; }
+        public decimal Weight { get; private set; }
+        public int CaloriesConsumed { get; private set; }
+        public int StepCount { get; private set; }
+        public int FatigueScore { get; private set; }
+        public string? Notes { get; private set; }
+       
+    }
+}
