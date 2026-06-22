098f4d20961dcfb9fb6a2829dbe71505f2fee86d
melo <melih@DESKTOP-MRA5AHM>
2026-06-22 22:54:19 +0300

    Sporculara abonelik baslangic ve bitis tarihleri eklendi

---
+++ SmartCoaching.Infrastructure/Migrations/20260622191714_AddSubscriptionDatesToAthlete.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartCoaching.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSubscriptionDatesToAthlete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "SubscriptionEndDate",
+                table: "Athletes",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "SubscriptionStartDate",
+                table: "Athletes",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SubscriptionEndDate",
+                table: "Athletes");
+
+            migrationBuilder.DropColumn(
+                name: "SubscriptionStartDate",
+                table: "Athletes");
+        }
+    }
+}
