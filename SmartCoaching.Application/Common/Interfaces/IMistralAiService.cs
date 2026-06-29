ad8747ddbd3d6ed02eadd39e111243de4b83923f
melo <melih@DESKTOP-MRA5AHM>
2026-06-30 00:18:19 +0300

    AI servislerindeki kullanılmayan ölü kodların (dead code) temizlenmesi
    IAiService içerisindeki kullanılmayan GenerateInsightAsync ve GenerateWeeklyAnalysisAsync metod tanımları ile MistralAiService içerisindeki gövdeleri silindi. Ayrıca projede hiçbir işlevi kalmayan IMistralAiService arayüzü tamamen kaldırıldı. Kod tabanı rahatlatıldı.

--- SmartCoaching.Application/Common/Interfaces/IMistralAiService.cs
+++
@@ -1,8 +0,0 @@
-using System.Threading.Tasks;
-
-namespace SmartCoaching.Application.Common.Interfaces;
-
-public interface IMistralAiService
-{
-    Task<string> GenerateWeeklyAnalysisAsync(string prompt);
-}
